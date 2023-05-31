1ad6b564177b52406e2ae55cb4b9954d4dc5ef8e
yj1438 <yj1438@163.com>
2023-05-31 15:21:09 +0800

    update: 调整 tsconfig; 补充 readme

--- tests/gltf_texture_transform.js
+++ tests/gltf_texture_transform.js
@@ -3,7 +3,7 @@ import { parse, textureTransform, getIOinstance } from '../dist/lib/index.js';
 
 async function fn (source) {
   const document = await parse(path.resolve(source), 'document');
-  textureTransform(document);
+  textureTransform(document, {resize: 512, format: 'webp'});
   //
   const io = getIOinstance();
   await io.write(path.resolve(source.replace('model', 'dist').replace('gltf', 'glb')), document);
