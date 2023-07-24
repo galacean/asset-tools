95176179cb011270d48fcd8a1c70ea98428ac8c1
沐瑜 <yj138290@antfin.com>
2023-07-24 17:00:16 +0800

    update: lock sharp version to 0.32.2

--- tests/gltf_texture_transform.js
+++ tests/gltf_texture_transform.js
@@ -1,12 +1,12 @@
 import path from 'path';
-import { parse, textureTransform, getIOinstance } from '../dist/lib/index.js';
+import { GltfTools } from '../dist/lib/index.js';
 
 async function fn (source) {
-  const document = await parse(path.resolve(source), 'document');
-  textureTransform(document, {resize: 512, format: 'webp'});
+  const document = await GltfTools.parse(path.resolve(source), 'document');
+  GltfTools.textureTransform(document, {resize: 512, format: 'webp'});
   //
-  const io = getIOinstance();
-  await io.write(path.resolve(source.replace('model', 'dist').replace('gltf', 'glb')), document);
+  const io = GltfTools.getIOinstance();
+  await io.write(path.resolve(source.replace('model', 'output').replace('gltf', 'glb')), document);
 }
 
 const temp = {
